eaab83d50ae6da231d9f361f72753a5baec0b3d3
Joyo-G <gustavo.joyo.p@gmail.com>
2024-05-27 12:10:48 -0400

    ADD: documentation

--- include/Tarea/heap.hpp
+++ include/Tarea/heap.hpp
@@ -6,14 +6,45 @@
 
 using namespace std;
 
+/**
+ * @class Heap
+ * @brief Clase que implementa una cola de prioridad utilizando la estructura heap
+ */
 class Heap : public PriorityQueue{
 private:
-    //variables
+    vector<pair<int,int>> heapData;
 public:
+    /**
+     * @brief Constructor por defecto de la clase Heap
+     */
     Heap();
+
+    /**
+     * @brief Constructor auxiliar para inicializar el heap con el heapifyTarget
+     * @param heapifyTarget Vector en donde se realizará la estructura heap
+     */
     Heap(vector<pair<int,int>> heapifyTarget);
+
+    /**
+     * @brief Esta función se encarga de encontrar el par minimo.
+     * @return Retorna el contenido el menor par
+     */
     pair<int,int> getMin() override;
+    /**
+     * @brief Esta función se encarga de encontrar el par minimo y sacarlo de la estructura
+     * @return Extrae y retorna el par minimo
+     */
     pair<int,int> extractMin() override;
+    /**
+     * @brief Función que se encarga de meter un nuevo par a la estructura.
+     * @param pair Par de datos a guardar
+     */
+    void putPair(pair<int,int> pair);
+
+    /**
+     * @brief Esta función se encarga de encontrar un nodo dentro de la estructura y disminuir su valor de la distancia
+     * @param node nodo a encontrar en la estructura
+     */
     void decreaseKey(int node) override;
 };
 
