5a6e6d66c3b9cee6f53993f8f96c228d42c80ada
Joyo-G <gustavo.joyo.p@gmail.com>
2024-05-27 12:11:32 -0400

    ADD: Heap vector constructor implementation

--- src/Heap.cpp
+++ src/Heap.cpp
@@ -1,7 +1,19 @@
 #include <Tarea/heap.hpp>
+#include <utility>
 
-Heap::Heap() {}
-Heap::Heap(vector<pair<int, int>> heapifyTarget) {}
-pair<int, int> Heap::getMin() {}
+Heap::Heap() = default;
+Heap::Heap(vector<pair<int, int>> heapifyTarget) {
+    heapData = std::move(heapifyTarget);
+}
+void Heap::putPair(pair<int, int> pair) {
+    heapData.push_back(pair);
+}
+
+pair<int, int> Heap::getMin() {
+    /*
+     * Metodo necesario para extraer el minimo
+     */
+}
 pair<int, int> Heap::extractMin() {}
+
 void Heap::decreaseKey(int node) {}
