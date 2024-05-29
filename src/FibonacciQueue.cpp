0d411ff0288976279793964507a8565b879f803b
javiromeroo <javiera.romero.o@ug.uchile.cl>
2024-05-29 16:56:50 -0400

    ADD: getMin implementation

--- src/FibonacciQueue.cpp
+++ src/FibonacciQueue.cpp
@@ -1,7 +1,20 @@
 #include <Tarea/fibonacciQueue.hpp>
 
 FibonacciQueue::FibonacciQueue() {}
+
+
 void FibonacciQueue::heapify(vector<pair<int, int>> target) {}
-pair<int, int> FibonacciQueue::getMin() {}
-pair<int, int> FibonacciQueue::extractMin() {}
+
+pair<int, int> FibonacciQueue::getMin() {
+    if(min == nullptr) {
+        return{INFINITY, -1};
+    }
+    return{min->distance, min->key};
+}
+
+pair<int, int> FibonacciQueue::extractMin() {
+
+}
+
+
 void FibonacciQueue::decreaseKey(int node) {}
