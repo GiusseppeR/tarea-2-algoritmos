10669e340dbde1744781f78b71d62229cdadae99
javiromeroo <javiera.romero.o@ug.uchile.cl>
2024-05-30 00:21:23 -0400

    ADD: insert function for heapify and start extractmin implementation

--- src/FibonacciQueue.cpp
+++ src/FibonacciQueue.cpp
@@ -3,7 +3,22 @@
 FibonacciQueue::FibonacciQueue(): min(nullptr), c(0) {}
 
 void FibonacciQueue:: insert(int node, int dist) {
-
+    FibonacciNode* insertNode= new FibonacciNode(node, dist);
+    elements[node] = insertNode;
+    if(min==nullptr) {//caso vacio
+        min=insertNode;
+    }
+    else {
+        insertNode->l=min;
+        insertNode->r=min->r;
+        min->r->l=insertNode;
+        min -> r=insertNode;
+        if(min->distance>insertNode->distance) {
+            //actualizamos min si es necesario
+            min=insertNode;
+        }
+    }
+    c++;
 }
 
 void FibonacciQueue::heapify(vector<pair<int, int>> target) {
@@ -20,7 +35,27 @@ pair<int, int> FibonacciQueue::getMin() {
 }
 
 pair<int, int> FibonacciQueue::extractMin() {
+    FibonacciNode* currMin = min;
+    if(currMin != nullptr) {
+        if(currMin->child != nullptr) {
+            FibonacciNode* minChild= currMin->child;
+            do {
+                FibonacciNode* next = minChild -> r;
+                minChild->l->r=minChild->r;
+                minChild->r->l = minChild-> l;
 
+                minChild->l=currMin;
+                minChild->r= currMin->r;
+
+                currMin->r->l=minChild;
+                currMin->r=minChild;
+                minChild->parent=nullptr;
+                minChild=next;
+            }while(minChild != currMin->child);
+        }
+
+
+    }
 }
 
 
@@ -30,7 +65,6 @@ void FibonacciQueue::decreaseKey(int node) {}
 void FibonacciQueue::cut(FibonacciNode* x, FibonacciNode* y) {
     if(x -> r == x && x -> l == x) { //caso en q x es el unico hijo
         y-> child = nullptr;
-
     }
     else {
         x->r->l = x ->l;
